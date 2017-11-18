b6321e64201eb6e063b36ed18ae50df145198fd2
dcwik96 <greek96@interia.pl>
2017-11-18 09:53:37 +0100

    Corrections in tests and refactoring

--- src/main/java/com/example/jdbcdemo/service/BookManager.java
+++ src/main/java/com/example/jdbcdemo/service/BookManager.java
@@ -6,21 +6,21 @@ import com.example.jdbcdemo.domain.Book;
 
 public interface BookManager {
 
-	public int addBook(Book book);
-	public void addAllBooks(List<Book> books);
+	int addBook(Book book);
+	void addAllBooks(List<Book> books);
 	
-	public int deleteBook(Book book);
-	public void deleteAllBooks();
+	int deleteBookById(Long id);
+	void deleteAllBooks();
 	
-	public int updateWholeBook(Book book, String title, String author, double price);
-	public int updateBookTitle(Book book, String title);
-	public int updateBookAuthor(Book book, String author);
-	public int updateBookPrice(Book book, double price);
+	int updateWholeBook(Book book, String title, String author, double price);
+	int updateBookTitle(Book book, String title);
+	int updateBookAuthor(Book book, String author);
+	int updateBookPrice(Book book, double price);
 	
-	public List<Book> getAllBooks();
-	public List<Book> findByTitle(String title);
-	public List<Book> findByAuthor(String title);
-	public Book findById(long id);
-	public List<Book> findByPrice(double price);
+	List<Book> getAllBooks();
+	List<Book> findByTitle(String title);
+	List<Book> findByAuthor(String title);
+	Book findById(long id);
+	List<Book> findByPrice(double price);
 	
 }
--- src/test/java/com/example/jdbcdemo/service/BookManagerJDBCTest.java
+++ src/test/java/com/example/jdbcdemo/service/BookManagerJDBCTest.java
@@ -1,15 +1,15 @@
 package com.example.jdbcdemo.service;
 
 import com.example.jdbcdemo.domain.Book;
-
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.either;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 
 public class BookManagerJDBCTest {
 
@@ -31,6 +31,7 @@ public class BookManagerJDBCTest {
 
     private BookManagerJDBC bookManager = new BookManagerJDBC();
 
+
     @Test
     public void checkConnection() {
         assertNotNull(bookManager.getConnection());
@@ -45,33 +46,35 @@ public class BookManagerJDBCTest {
         assertEquals(true, books.isEmpty());
     }
 
+
     @Test
     public void checkAdding() {
 
         assertEquals(1, bookManager.addBook(book));
 
         List<Book> books = bookManager.getAllBooks();
-        Book bookRetrived = books.get(0);
+        Book bookRetrieved = books.get(0);
 
-        assertEquals(book.getTitle(), bookRetrived.getTitle());
-        assertEquals(book.getAuthor(), bookRetrived.getAuthor());
-        assertEquals(book.getPrice(), bookRetrived.getPrice(), 0.01);
+        assertEquals(1, bookManager.getAllBooks().size());
+
+        assertEquals(book.getTitle(), bookRetrieved.getTitle());
+        assertEquals(book.getAuthor(), bookRetrieved.getAuthor());
+        assertEquals(book.getPrice(), bookRetrieved.getPrice(), 0.01);
     }
-    
-    //OK
+
     @Test
     public void checkAddAllBooks() {
-    	List<Book> books = new ArrayList<>();
-    	books.add(book);
-    	books.add(book2);
-    	books.add(book3);
-    	
-    	bookManager.addAllBooks(books);
-    	int size = bookManager.getAllBooks().size();
-    	assertThat(size, either(is(3)).or(is(0)));
+        List<Book> books = new ArrayList<>();
+        books.add(book);
+        books.add(book2);
+        books.add(book3);
+
+        bookManager.addAllBooks(books);
+        int size = bookManager.getAllBooks().size();
+        assertThat(size, either(is(3)).or(is(0)));
     }
 
-	@Test
+    @Test
     public void checkFindById() {
 
         bookManager.addBook(book);
@@ -79,11 +82,11 @@ public class BookManagerJDBCTest {
         List<Book> books = bookManager.getAllBooks();
         Book tmp = books.get(0);
 
-        Book bookRetrived = bookManager.findById(tmp.getId());
+        Book bookRetrieved = bookManager.findById(tmp.getId());
 
-        assertEquals(book.getTitle(), bookRetrived.getTitle());
-        assertEquals(book.getAuthor(), bookRetrived.getAuthor());
-        assertEquals(book.getPrice(), bookRetrived.getPrice(), 0.01);
+        assertEquals(book.getTitle(), bookRetrieved.getTitle());
+        assertEquals(book.getAuthor(), bookRetrieved.getAuthor());
+        assertEquals(book.getPrice(), bookRetrieved.getPrice(), 0.01);
     }
 
     @Test
@@ -106,6 +109,7 @@ public class BookManagerJDBCTest {
         bookManager.addBook(book2);
 
         List<Book> books = bookManager.findByAuthor(book2.getAuthor());
+        assertEquals(2, books.size());
         Book bookRetrived = books.get(0);
 
         assertEquals(book2.getAuthor(), bookRetrived.getAuthor());
@@ -129,9 +133,12 @@ public class BookManagerJDBCTest {
         bookManager.addBook(book);
         bookManager.addBook(book2);
 
-        assertEquals(1, bookManager.deleteBook(book2));
-
         List<Book> books = bookManager.getAllBooks();
+        Book tmp = books.get(1);
+
+        assertEquals(1, bookManager.deleteBookById(tmp.getId()));
+
+        books = bookManager.getAllBooks();
         Book bookRetrived = books.get(0);
 
         assertEquals(1, books.size());
@@ -193,6 +200,6 @@ public class BookManagerJDBCTest {
         List<Book> books = bookManager.getAllBooks();
         Book bookRetrived = books.get(1);
 
-		assertEquals(PRICE_3, bookRetrived.getPrice(), 0.01);
-	}
+        assertEquals(PRICE_3, bookRetrived.getPrice(), 0.01);
+    }
 }
